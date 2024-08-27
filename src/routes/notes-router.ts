26036f5fb34c6911b87e5ac1268e137685d3bdea
Yehia Mohamed <yehiamohamed915@yahoo.com>
2024-08-27 17:52:31 +0200

    fix a samll bug

--- src/routes/notes-router.ts
+++ src/routes/notes-router.ts
@@ -4,6 +4,8 @@ import updateNote from "../controllers/notes-controller/update-note";
 import {deleteNote} from "../controllers/notes-controller/delete-note";
 import {getSingleNote} from "../controllers/notes-controller/get-note";
 import{addTagToNote} from "../controllers/notes-controller/add-tag-to-note";
+import{removeTagFromNote} from "../controllers/notes-controller/delete-tag-from-note";
+
 
 import express from 'express';
 
@@ -21,6 +23,6 @@ router.get('/notes/:id',getSingleNote);
 
 router.post('/notes/:noteId/tags/:tagId',addTagToNote)
 
-router.delete('/notes/:noteId/tags/:tagId',addTagToNote)
+router.delete('/notes/:noteId/tags/:tagId',removeTagFromNote);
 
 export default router;
